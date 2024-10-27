ef541f9badffb9b0354bbb32d4f4006512672250
v.zakharov <v.zakharov@mts.ai>
2024-10-27 15:46:24 +0300

    Initial Commit

---
+++ Hello QA GuruTests/Hello_QA_GuruTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_QA_GuruTests.swift
+//  Hello QA GuruTests
+//
+//  Created by v.zakharov on 27.10.2024.
+//
+
+import Testing
+@testable import Hello_QA_Guru
+
+struct Hello_QA_GuruTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
